93d762f602241bab5500566b1ad5839088c183d5
BabakBar <babak.barghi@gmail.com>
2022-04-14 10:01:40 +0200

    sql alchemy config

--- ToDoApp_Flask/app.py
+++ ToDoApp_Flask/app.py
@@ -3,6 +3,11 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+# /// = relative path, //// = absolute path
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
 @app.route('/') #decorator - homepage
 def index():    # assign a method - response to request
     return 'Hello from Sia!'
