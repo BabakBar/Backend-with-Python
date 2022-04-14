a02a2444be2b8ff37c74517b67f2316c98352548
BabakBar <babak.barghi@gmail.com>
2022-04-14 10:02:32 +0200

    define class

--- ToDoApp_Flask/app.py
+++ ToDoApp_Flask/app.py
@@ -8,6 +8,11 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    complete = db.Column(db.Boolean)
+
 @app.route('/') #decorator - homepage
 def index():    # assign a method - response to request
     return 'Hello from Sia!'
