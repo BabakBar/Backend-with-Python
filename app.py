13a39556f1e11335519e5c58aece42a27d097d9a
BabakBar <babak.barghi@gmail.com>
2022-04-14 02:53:48 +0200

    web server

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/') #decorator - homepage
-def index():    #method - response to request
+def index():    # assign a method - response to request
     return 'Hello from Sia!'
 
 app.run(port=5000)
