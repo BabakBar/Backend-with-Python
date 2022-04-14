b3f8a082b67d6dfb08d7537d92e89c6e3aa5b024
BabakBar <babak.barghi@gmail.com>
2022-04-14 10:05:49 +0200

    class comment

--- ToDoApp_Flask/app.py
+++ ToDoApp_Flask/app.py
@@ -8,6 +8,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+# a class to inherit from db.Model with 3 columns & data types
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
